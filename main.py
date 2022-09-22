040cd3c7043f8de11c772e97bb255ee27076a156
Mahyar Sahraei <sahraei19@gmail.com>
2022-09-23 02:54:28 +0330

    minor change

--- main.py
+++ main.py
@@ -2,8 +2,10 @@ from board import Board
 from visualizer import Visualizer
 
 if __name__ == '__main__':
-    # main_board = Board('sets/standard/std_board.json')
-    # visualizer = Visualizer(main_board, 'sets/standard/std_setting.json')
-    main_board = Board('sets/custom1/cst1_board.json')
-    visualizer = Visualizer(main_board, 'sets/custom1/cst1_setting.json')
+    #paths to required json configurations
+    board_config = 'sets/standard/std_board.json'
+    setting_config = 'sets/standard/std_setting.json'
+
+    main_board = Board(board_config)
+    visualizer = Visualizer(main_board, setting_config)
     visualizer.start()
