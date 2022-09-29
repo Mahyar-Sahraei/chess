6dad58e201f8bd467ff8f1a154b88b8f9c319cb1
Mahyar Sahraei <sahraei19@gmail.com>
2022-09-29 05:55:37 +0330

    added color switcher

--- color.py
+++ color.py
@@ -4,3 +4,8 @@ class ColorT(Enum):
     NONE = 0,
     WHITE = 1,
     BLACK = 2
+
+    def switch_color(color):
+        if color == ColorT.BLACK:
+            return ColorT.WHITE
+        return ColorT.BLACK
